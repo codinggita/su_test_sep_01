7b2e7139db01dd827a00f5eb769c43a0a1ce49df
Hetavi Panchotia <hetavipanchotia@gmail.com>
2025-09-01 16:07:59 +0530

    108563

--- C_Test/Q2.c
+++ C_Test/Q2.c
@@ -1,43 +1,27 @@
 #include <stdio.h>
 int main(){
-    int unit,bill=100;
+    int unit,bill;
     scanf("%d",&unit);
-    int temp = unit;
-    // while (temp>0){
-    //     if(temp>=50){
-    //         bill=50*2;
-    //         temp=temp-50;
-    //         if(temp>=50){
-    //             bill=bill+ (50*3);
-    //             temp= temp-50;
-    //             if (temp>=50){
-    //                 bill=bill + (50*4);
-    //                 temp=temp-50;
-    //                 if(temp>=50){
-    //                 bill= bill + (50*5);
-    //                 temp=temp-50;
-    //                 if (temp>=50){
-    //                 bill =bill + (50*6);
-    //                 temp=temp-50;
-    //                 if (temp>=50){
-    //                     bill = bill + (50*8);
-    //                 }
-    //             }
-    //             else {
-    //         bill = (temp%50)*6;
-    //     }
-    //      else {
-    //         bill = (temp%50)*4;
-    //     }
-    //         }
-    //         else {
-    //         bill = (temp%50)*3;
-    //     }
-    //     }
-    //     else {
-    //         bill = (temp%50)*2
-    //     }
-    // }
+    if (unit>=50 && unit<=50){
+        bill= 100 + (unit*2);
+    }
+    else if (unit >=51 && unit<=100){
+        bill= 100 + (50*2) + ((unit-50) *3);
+    }
+    else if (unit >=101 && unit<=200){
+        bill= 100 + (50*2) + (50*3) +((unit-100) *4);
+    }
+    else if (unit >=201 && unit<=300){
+        bill= 100 + (50*2) + (50*3)+ (50*4)+ ((unit-150) *5);
+    }
+    else if (unit >=301 && unit<=500){
+        bill= 100 + (50*2) + (50*3)+ (50*4) + (50*5) +((unit-200) *6);
+    }
+    else {
+        bill= 100 + (50*2) + (50*3)+ (50*4) + (50*5) + (50*6)+((unit-250) *8);
+    }
 printf("%d",bill);
+
+
     return 0;
 }

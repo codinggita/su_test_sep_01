2081cdba293af6ae6a1f0f9f7fe62306616fcf07
ARJUN DUBEY <arjundubey.cg@gmail.com>
2025-09-01 17:23:58 +0530

    ARJUN DUBEY

---
+++ test/Q6.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+int main (){
+
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        sum += num % 10; 
+        num /= 10;        
+    }
+
+    printf("Sum of Digits = %d\n", sum);
+
+   
+
+
+
+    return 0;
+}
